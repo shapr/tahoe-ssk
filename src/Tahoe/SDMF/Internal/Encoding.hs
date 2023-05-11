e942ff5778aeff43312ddbc5d1aaaf18e9c9fc4e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-05-11 11:45:11 -0400

    Push Maybes into Eithers instead of MonadFails

--- src/Tahoe/SDMF/Internal/Encoding.hs
+++ src/Tahoe/SDMF/Internal/Encoding.hs
@@ -3,15 +3,15 @@
 module Tahoe.SDMF.Internal.Encoding where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
-import qualified Data.ByteString.Lazy as LB
-import Data.Word (Word16, Word64, Word8)
-
 import Crypto.Cipher.AES128 (AESKey128, BlockCipher (buildKey))
 import Crypto.Classes (getIVIO)
 import qualified Crypto.PubKey.RSA.Types as RSA
 import Crypto.Types (IV)
 import Data.Bifunctor (Bifunctor (bimap))
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.Text as T
+import Data.Word (Word16, Word64, Word8)
 import Tahoe.CHK (padCiphertext, zfec, zunfec)
 import Tahoe.CHK.Crypto (taggedHash)
 import Tahoe.CHK.Encrypt (encrypt)
@@ -28,24 +28,33 @@ import Tahoe.SDMF.Internal.Share (HashChain (HashChain), Share (..), signatureKe
 -}
 encode :: (MonadFail m, MonadIO m) => RSA.KeyPair -> Word64 -> Word16 -> Word16 -> LB.ByteString -> m ([Share], Writer)
 encode keypair shareSequenceNumber required total ciphertext = do
-    -- XXX Ciphertext needs to be padded to a multiple of required right?
-    blocks <- liftIO $ zfec (fromIntegral required) (fromIntegral total) (LB.toStrict $ padCiphertext required ciphertext)
+    blocks <- liftIO $ fmap LB.fromStrict <$> zfec (fromIntegral required) (fromIntegral total) (LB.toStrict $ padCiphertext required ciphertext)
 
     -- XXX Secure enough random source?
     iv <- liftIO (getIVIO :: IO (IV AESKey128))
 
     -- XXX fromIntegral is going from Word16 to Word8, not safe
-    makeShare' <- makeShare shareSequenceNumber iv (fromIntegral required) (fromIntegral total) (fromIntegral $ LB.length ciphertext) (RSA.toPublicKey keypair) <$> encryptedPrivateKey
+    let makeShareE =
+            makeShare
+                shareSequenceNumber
+                iv
+                (fromIntegral required)
+                (fromIntegral total)
+                (fromIntegral $ LB.length ciphertext)
+                (RSA.toPublicKey keypair)
+                <$> encryptedPrivateKey
 
-    let shares = makeShare' . LB.fromStrict <$> blocks
-    (shares,) <$> cap
+    let resultE = map <$> makeShareE <*> pure blocks
+    case (,) <$> resultE <*> cap of
+        Left err -> fail (T.unpack err)
+        Right result -> pure result
   where
     -- We can compute a capability immediately.
     cap = capabilityForKeyPair keypair
     encryptedPrivateKey = flip encryptPrivateKey (RSA.toPrivateKey keypair) <$> (writerWriteKey <$> cap)
 
 encryptPrivateKey :: AESKey128 -> RSA.PrivateKey -> B.ByteString
-encryptPrivateKey key = LB.toStrict . encrypt key . LB.fromStrict . signatureKeyToBytes
+encryptPrivateKey writeKey = LB.toStrict . encrypt writeKey . LB.fromStrict . signatureKeyToBytes
 
 makeShare ::
     Word64 ->
@@ -76,21 +85,21 @@ decode _ s@((_, Share{shareRequiredShares, shareTotalShares, shareSegmentSize}) 
     blocks = bimap fromIntegral (LB.toStrict . shareData) <$> s
 
 -- | Compute an SDMF write capability for a given keypair.
-capabilityForKeyPair :: MonadFail m => RSA.KeyPair -> m Writer
+capabilityForKeyPair :: RSA.KeyPair -> Either T.Text Writer
 capabilityForKeyPair keypair =
     Writer <$> writerWriteKey <*> writerReader
   where
-    writerWriteKey = deriveWriteKey . RSA.toPrivateKey $ keypair
+    writerWriteKey = maybeToEither "Failed to derive write key" . deriveWriteKey . RSA.toPrivateKey $ keypair
     verificationKeyHash = hashVerificationKey . RSA.toPublicKey $ keypair
     writerReader = deriveReader <$> writerWriteKey <*> pure verificationKeyHash
 
 -- | Compute the write key for a given signature key for an SDMF share.
-deriveWriteKey :: MonadFail m => RSA.PrivateKey -> m AESKey128
-deriveWriteKey privKey = do
-    let key = buildKey . taggedHash writeKeyLength mutableWriteKeyTag . signatureKeyToBytes $ privKey
-    case key of
-        Nothing -> fail "Couldn't build AESKey128"
-        Just k -> pure k
+deriveWriteKey :: RSA.PrivateKey -> Maybe AESKey128
+deriveWriteKey = buildKey . taggedHash writeKeyLength mutableWriteKeyTag . signatureKeyToBytes
+
+maybeToEither :: a -> Maybe b -> Either a b
+maybeToEither a Nothing = Left a
+maybeToEither _ (Just b) = Right b
 
 {- | The tag used when hashing the signature key to the write key for the
  creation of an SDMF capability.
