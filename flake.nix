5a5e5538812505e9dcd19544311b077561447bf0
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-08-15 14:43:38 -0400

    better test runner

--- flake.nix
+++ flake.nix
@@ -89,10 +89,12 @@
             runtimeInputs = with pkgs; [pkg-config haskell.compiler.${ghcVersion} cabal-install];
 
             text = ''
+              set -ex
               nix run .#generate-cabal-project
               cabal update hackage.haskell.org
-              cabal build all
-              cabal run tests
+              cabal build --enable-tests
+              runtests=$(cabal list-bin --enable-tests tahoe-ssk-test)
+              eval "$runtests"
             '';
           }
         }/bin/cabal-build-and-test";
